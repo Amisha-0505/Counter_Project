1d895c2043ffaf4df83dcb2c08e1bd02dd813795
Amisha-0505 <amishashaw375@gmail.com>
2025-11-04 21:08:17 +0530

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <div className='flex justify-center items-center h-screen text-3xl font-bold' >
+      <button className="bg- text-white px- py-3 rounded-lg text-xl font-semibold hover:bg-blue-700 transition">
+        Counter
+      </button>
+    </div>
+  )
+}
+
+export default Counter
